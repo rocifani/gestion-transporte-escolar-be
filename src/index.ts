eed207276bc9704dd2c174f4eeaaacc1a3af495b
AgustinaRodriguez01 <agus4956@gmail.com>
2025-03-25 22:47:04 -0300

    add HTTPS support with self-signed certificate

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import AppDataSource from "./database/db";
 import routes from "./routes/routes";
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 
 dotenv.config();
-
 
 const app = express();
 app.use(express.json());
@@ -13,12 +14,21 @@ app.use(cors());
 app.use("/", routes);
 
 const PORT = process.env.PORT || 3000;
+const HTTPS_PORT = 3443;
+
+const httpsOptions = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+}
 
 AppDataSource.initialize()
   .then(() => {
     console.log("Database connected!");
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
+    });
+    https.createServer(httpsOptions, app).listen(HTTPS_PORT, () => {
+      console.log(`Server running on HTTPS: https://localhost:${HTTPS_PORT}`);
     });
   })
   .catch((error) => console.error(error));
